04cff07e998327ee96e48123bb1e3522d3b03bd2
Rodro-Kumar <rudrokormokar96@gmail.com>
2023-12-25 14:49:05 +0600

    major update

--- main.js
+++ main.js
@@ -10,11 +10,21 @@ function generateBtn() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.ceil(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
-  let generateColor = `rgb (${red} , ${green} , ${blue})`;
+  let generateColor = `rgb(${red} , ${green} , ${blue})`;
   body.style.background = generateColor;
+  colorCode.style.color = generateColor;
   show(generateColor);
 }
 
 function show(code) {
   colorCode.value = code;
+}
+
+copyBtn.addEventListener("click", copyColor);
+
+function copyColor() {
+  navigator.clipboard.writeText(colorCode.value);
+  copyBtn.innerHTML = "copied";
+  copyBtn.style.background = "#FF9800";
+  copyBtn.style.color = "#304D30";
 }
